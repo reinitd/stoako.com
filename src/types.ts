7b738890c6f264aab73e2aab325dd6af86d335c8
reinitd <russian.devv@gmail.com>
2024-09-29 00:24:37 -0500

    added example bnr

--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,42 @@
 export type AuthHeader = {
-    headers: {
-        Authorization: string;
-    };
+  headers: {
+    Authorization: string;
+  };
 };
+
+export type PropositionDocument = {
+  title: string;
+  prop: string;
+  data: string;
+  articleCount: string;
+  reason: string;
+  proposedBy: string[];
+  ministryOf: string | null;
+  billOrResolution: string;
+  approvedStatus: string;
+  resources: {
+    identifier: string;
+    docLink: string;
+  }[];
+  votes: {
+    yes: string[];
+    no: string[];
+  };
+  docLink: string;
+};
+
 
 export type Citizen = {
-    name: string;
-    roles: CitizenRole[];
-    joinDate: string;
+  name: string;
+  roles: {
+    role: string,
+    of: string
+  }[];
+  joinDate: string;
 };
 
-export type CitizenRole = {
-  role: string,
-  of: string
-}
-
 export type CitizensList = {
-    citizens: Citizen[];
+  citizens: Citizen[];
 };
 
 export type WatchlistEntry = {
